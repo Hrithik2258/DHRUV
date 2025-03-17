1b01c5e3dbceb57039a519916b0921c932db1a55
Hrihtik Pawar <86373306+Hrithik2258@users.noreply.github.com>
2025-03-18 00:20:30 +0530

    Update power.js

--- routes/power.js
+++ routes/power.js
@@ -23,13 +23,13 @@ router.get("/getall", async (req, res) => {
 // 2️⃣ **POST - Add a Power Record**
 router.post("/add", async (req, res) => {
   try {
-    const { Voltage, Frequency, THD Voltage, Harmonic 3rd, Harmonic 5th,Harmonic 7th } = req.body;
+    const { voltage, frequency, THD_Voltage_1N, Harmonic_3rd, Harmonic_5th,Harmonic_7th } = req.body;
 
-    if (!Voltage || !Frequency || !THD Voltagee || !Harmonic 3rd || !Harmonic 5th,Harmonic 7th ) {
+    if (!Voltage || !Frequency || !THD Voltagee || !Harmonic_3rd || !Harmonic_5th,Harmonic_7th ) {
       return res.status(400).json({ error: "All fields are required" });
     }
 
-    const newPower = new Power({ Voltage, Frequency, THD Voltage, Harmonic 3rd, Harmonic 5th, Harmonic 7th});
+    const newPower = new Power({ voltage, frequency, THD_Voltage_1N, Harmonic_3rd, Harmonic_5th, Harmonic_7th});
     await newPower.save();
 
     res.json({ message: "Power data added successfully", data: newPower });
