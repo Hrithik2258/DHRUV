420da78f25039aa9539230cf651eda74317f18e6
Hrihtik Pawar <86373306+Hrithik2258@users.noreply.github.com>
2025-03-20 13:06:31 +0530

    Update Power.js

--- models/Power.js
+++ models/Power.js
@@ -4,9 +4,9 @@ const PowerSchema = new mongoose.Schema({
   voltage: { type: Number, required: true },
   frequency: { type: Number, required: true },
   THD_voltage_1N: { type: Number, required: true },
-  harmonic_3rd: { type: Number, required: true },
-  harmonic_5th: { type: Number, required: true },
-  harmonic_7th: { type: Number, required: true },
+  harmonic_3rd: { type: Number },
+  harmonic_5th: { type: Number },
+  harmonic_7th: { type: Number },
   timestamp: { type: Date, default: Date.now },
 });
 
